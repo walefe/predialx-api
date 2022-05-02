0533d7688c320b1f77718b36f8f8ef28d7c8aece
Walefe de Jesus <walefedev@gmail.com>
2022-05-02 18:53:42 -0300

    chore: add prisma and create migrations for customers.

---
+++ prisma/migrations/20220502215015_table_customers/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "customer" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "customer_pkey" PRIMARY KEY ("id")
+);
